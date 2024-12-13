5ecfc08d3352f929b64541c82d47fb1ac185b1c3
bhavya1099 <bhavya.goswami99@gmail.com>
2024-12-13 11:02:02 +0530

    Unit test generated by RoostGPT
    Using AI Model gpt-4

---
+++ src/test/java/com/visualpathit/account/model/Categories.java
@@ -0,0 +1,121 @@
+package com.visualpathit.account.model;
+
+public interface Categories {
+
+	interface valid {
+
+	}
+
+	interface invalid {
+
+	}
+
+	interface boundary {
+
+	}
+
+	interface integration {
+
+	}
+
+	interface getId {
+
+	}
+
+	interface getName {
+
+	}
+
+	interface getUsers {
+
+	}
+
+	interface getUsername {
+
+	}
+
+	interface getPassword {
+
+	}
+
+	interface getUserEmail {
+
+	}
+
+	interface getPasswordConfirm {
+
+	}
+
+	interface getRoles {
+
+	}
+
+	interface getProfileImg {
+
+	}
+
+	interface getProfileImgPath {
+
+	}
+
+	interface getDateOfBirth {
+
+	}
+
+	interface getFatherName {
+
+	}
+
+	interface getMotherName {
+
+	}
+
+	interface getGender {
+
+	}
+
+	interface getMaritalStatus {
+
+	}
+
+	interface getPermanentAddress {
+
+	}
+
+	interface getTempAddress {
+
+	}
+
+	interface getPrimaryOccupation {
+
+	}
+
+	interface getSecondaryOccupation {
+
+	}
+
+	interface getSkills {
+
+	}
+
+	interface getPhoneNumber {
+
+	}
+
+	interface getSecondaryPhoneNumber {
+
+	}
+
+	interface getNationality {
+
+	}
+
+	interface getLanguage {
+
+	}
+
+	interface getWorkingExperience {
+
+	}
+
+}
